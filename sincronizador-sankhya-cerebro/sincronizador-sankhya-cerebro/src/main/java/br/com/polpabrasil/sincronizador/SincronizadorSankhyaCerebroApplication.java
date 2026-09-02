59a24ac80c19e83503f33a28786ecdf86c56cb48
Robson de Jesus <robsondejesus1996@hotmail.com>
2026-09-02 15:12:13 -0300

    feat: created project

---
+++ sincronizador-sankhya-cerebro/sincronizador-sankhya-cerebro/src/main/java/br/com/polpabrasil/sincronizador/SincronizadorSankhyaCerebroApplication.java
@@ -0,0 +1,13 @@
+package br.com.polpabrasil.sincronizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SincronizadorSankhyaCerebroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SincronizadorSankhyaCerebroApplication.class, args);
+	}
+
+}
---
+++ sincronizador-sankhya-cerebro/sincronizador-sankhya-cerebro/src/test/java/br/com/polpabrasil/sincronizador/SincronizadorSankhyaCerebroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.polpabrasil.sincronizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SincronizadorSankhyaCerebroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
